2402b0a25c73f87a7542c7693265dbfc6fd32c30
zulu27 <143005593+zulu27@users.noreply.github.com>
2023-10-21 21:36:25 -0500

    Add files via upload

---
+++ juego calculadora.py
@@ -0,0 +1,31 @@
+
+def add(num_1, num_2):
+  result = num_1 + num_2
+  print(f'{num_1} + {num_2} is equal to {result}')
+  return result
+
+def game():
+  score = 0
+  while True:
+    print('======== Menu ========'
+    '\n1. Add'
+    '\n0. Exit')
+    option = input('\nChoice an option: ')
+    if option == "0":
+      break
+    num_1 = int(input('Enter first number: '))
+    num_2 = int(input('Enter second number: '))
+    answer = int(input('Enter you answer: '))
+    if option == "1":
+        result = add(num_1, num_2)
+        if result == answer:
+          score += 1
+          print('Correct!!')
+        else:
+          print('Incorrect')
+
+  print(f'======== Game Over ========'
+  f'\nYou score is {score}'
+  '\nKeep going!')
+
+
