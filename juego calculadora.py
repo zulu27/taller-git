4d32ff3d8e0b105ab5dfca245630fc2cd6204b5d
zulu27 <143005593+zulu27@users.noreply.github.com>
2023-10-21 21:51:13 -0500

    Update juego calculadora.py
    se agrego la función resta

--- juego calculadora.py
+++ juego calculadora.py
@@ -1,4 +1,7 @@
-
+def resta(num_1, num_2):
+  result = num_1 - num_2
+  print(f'{num_1 + {num_2} is equal to {result}')
+  return result
 def add(num_1, num_2):
   result = num_1 + num_2
   print(f'{num_1} + {num_2} is equal to {result}')
