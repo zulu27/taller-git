ab38669d46c4a66bc8a9380d8610d4125906e890
zulu27 <143005593+zulu27@users.noreply.github.com>
2023-10-21 21:53:05 -0500

    Update juego calculadora.py

--- juego calculadora.py
+++ juego calculadora.py
@@ -2,6 +2,7 @@ def resta(num_1, num_2):
   result = num_1 - num_2
   print(f'{num_1 + {num_2} is equal to {result}')
   return result
+  
 def add(num_1, num_2):
   result = num_1 + num_2
   print(f'{num_1} + {num_2} is equal to {result}')
